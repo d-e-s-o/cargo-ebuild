d-e-s-o/cargo-ebuild

commit 75a2f5b83302b7b747cfb41e75d4d9b2adfff92f

fixed local crates being added to ebuild

src/lib.rs
@@ -55,7 +55,11 @@ pub fn gen_ebuild_data(manifest_path: Option<PathBuf>) -> Result<EbuildConfig> {
             root_pkg = Some(pkg.clone());
         }
 
-        crates.push(format!("{}-{}\n", pkg.name, pkg.version));
+        if let Some(src) = pkg.source {
+            if src.is_crates_io() {
+                crates.push(format!("{}-{}\n", pkg.name, pkg.version));
+            }
+        }
 
         if let Some(lic_list) = pkg.license.as_ref().map(|l| parse_license(&l)) {
             for lic in lic_list.iter() {

commit 6f67eb0c81ee85b1c40f38bd547ac5a829a50575

Add "own" crate back to list of dependent crates
Commit 75a2f5b83302b7b747cfb41e75d4d9b2adfff92f caused the program
itself to be added to the list of dependencies, which in turn meant that
Portage will not fetch that package and, hence, the source is not
available for compilation.
This change reverts said commit to preserve the desired behavior.

src/lib.rs
@@ -108,11 +108,7 @@ pub fn gen_ebuild_data(manifest_path: Option<PathBuf>) -> Result<EbuildConfig> {
     let lockfile = Lockfile::load(lockfile_path)?;
 
     for pkg in lockfile.packages {
-        if let Some(src) = pkg.source {
-            if src.is_default_registry() {
-                crates.push(format!("{}-{}\n", pkg.name, pkg.version));
-            }
-        }
+        crates.push(format!("{}-{}\n", pkg.name, pkg.version));
     }
 
     Ok(EbuildConfig::from_package(root_pkg, crates, licenses))